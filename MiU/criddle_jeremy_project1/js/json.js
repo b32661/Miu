69ece228d6cedf10d1c56bf259ab67a6b0dd58b3
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-03-07 20:55:54 -0500

    initilize

---
+++ MiU/criddle_jeremy_project1/js/json.js
@@ -0,0 +1,189 @@
+/**
+ *   Jeremy Criddle
+ *   MiU 1303
+ *   Project 1
+ *  
+ */
+ //json dummy data for Timestomper
+ var json = {
+	 "stomp1": {
+		 "group":["Group:", "End"],
+		 "sure":["Sure:", "6"],
+		 "format":["Format:", "Military"],
+		 "name":["Name:", "Drill"],
+		 "description":["Description:", "Fire drill"],
+		 "time":["Time:", "8:30"],
+		 "date":["Date:", "2/10/2013"]
+	 },
+	 "stomp2": {
+		 "group":["Group:", "Departure"],
+		 "sure":["Sure:", "8"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Game"],
+		 "description":["Description:", "Chicago Bears"],
+		 "time":["Time:", "11:45"],
+		 "date":["Date:", "12/12/2012"]
+	 },
+	 "stomp3": {
+		 "group":["Group:", "Arrival"],
+		 "sure":["Sure:", "3"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Airport"],
+		 "description":["Description:", "Chicago Midway"],
+		 "time":["Time:","2:15"],
+		 "date":["Date:","1/22/2013"]
+	 },
+	 "stomp4": {
+		 "group":["Group:", "Start"],
+		 "sure":["Sure:", "1"],
+		 "format":["Format:", "Military"],
+		 "name":["Name:", "Drill"],
+		 "description":["Description:", "Fire drill"],
+		 "time":["Time:", "7:30"],
+		 "date":["Date:", "2/10/2013"]
+	 },
+	 "stomp5": {
+		 "group":["Group:", "Departure"],
+		 "sure":["Sure:", "5"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Airport"],
+		 "description":["Description:", "MDW"],
+		 "time":["Time:", "1:45"],
+		 "date":["Date:", "2/1/2012"]
+	 },
+	 "stomp6": {
+		 "group":["Group:", "Arrival"],
+		 "sure":["Sure:", "9"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Home from MS"],
+		 "description":["Description:", "Rain was heavy"],
+		 "time":["Time:","2:15"],
+		 "date":["Date:","1/22/2011"]
+	 },
+	 "stomp7": {
+		 "group":["Group:", "Start"],
+		 "sure":["Sure:", "3"],
+		 "format":["Format:", "Military"],
+		 "name":["Name:", "Drum Core"],
+		 "description":["Description:", "Blue Demons"],
+		 "time":["Time:", "4:00"],
+		 "date":["Date:", "2/17/2013"]
+	 },
+	 "stomp8": {
+		 "group":["Group:", "Departure"],
+		 "sure":["Sure:", "9"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Drum Core"],
+		 "description":["Description:", "I-94"],
+		 "time":["Time:", "1:45"],
+		 "date":["Date:", "1/5/2013"]
+	 },
+	 "stomp9": {
+		 "group":["Group:", "Arrival"],
+		 "sure":["Sure:", "3"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Kentucky Derby"],
+		 "description":["Description:", "E5 parked"],
+		 "time":["Time:","2:15"],
+		 "date":["Date:","3/22/2013"]
+	 },
+	 "stomp10": {
+		 "group":["Group:", "Start"],
+		 "sure":["Sure:", "1"],
+		 "format":["Format:", "Military"],
+		 "name":["Name:", "Fireworks"],
+		 "description":["Description:", "Buchanan"],
+		 "time":["Time:", "8:10"],
+		 "date":["Date:", "2/10/2013"]
+	 },
+	 "stomp11": {
+		 "group":["Group:", "Departure"],
+		 "sure":["Sure:", "5"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Fireworks"],
+		 "description":["Description:", "Buchanan"],
+		 "time":["Time:", "9:45"],
+		 "date":["Date:", "2/10/2013"]
+	 },
+	 "stomp12": {
+		 "group":["Group:", "End"],
+		 "sure":["Sure:", "1"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Snowstorm"],
+		 "description":["Description:", "2 feet"],
+		 "time":["Time:","12:55"],
+		 "date":["Date:","3/2/2013"]
+	 },
+	 "stomp13": {
+		 "group":["Group:", "Departure"],
+		 "sure":["Sure:", "4"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Jannisport"],
+		 "description":["Description:", "Corsair"],
+		 "time":["Time:", "11:45"],
+		 "date":["Date:", "12/1/2012"]
+	 },
+	 "stomp14": {
+		 "group":["Group:", "Arrival"],
+		 "sure":["Sure:", "3"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Jannisport"],
+		 "description":["Description:", "From Chicago Midway"],
+		 "time":["Time:","2:15"],
+		 "date":["Date:","11/22/2012"]
+	 },
+	 "stomp15": {
+		 "group":["Group:", "Start"],
+		 "sure":["Sure:", "1"],
+		 "format":["Format:", "Military"],
+		 "name":["Name:", "Tourney"],
+		 "description":["Description:", "Magic the Gathering"],
+		 "time":["Time:", "1:30"],
+		 "date":["Date:", "1/1/2011"]
+	 },
+	 "stomp16": {
+		 "group":["Group:", "Departure"],
+		 "sure":["Sure:", "9"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Airport Hotel"],
+		 "description":["Description:", "Empress"],
+		 "time":["Time:", "11:42"],
+		 "date":["Date:", "2/12/2011"]
+	 },
+	 "stomp17": {
+		 "group":["Group:", "Arrival"],
+		 "sure":["Sure:", "1"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Caymen Islands"],
+		 "description":["Description:", "Beautiful"],
+		 "time":["Time:","2:15"],
+		 "date":["Date:","11/22/2013"]
+	 },
+	 "stomp18": {
+		 "group":["Group:", "Start"],
+		 "sure":["Sure:", "1"],
+		 "format":["Format:", "Military"],
+		 "name":["Name:", "Blues Show"],
+		 "description":["Description:", "nightlife"],
+		 "time":["Time:", "22:00"],
+		 "date":["Date:", "2/17/2013"]
+	 },
+	 "stomp19": {
+		 "group":["Group:", "Departure"],
+		 "sure":["Sure:", "9"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Florida"],
+		 "description":["Description:", "School Visit"],
+		 "time":["Time:", "9:45"],
+		 "date":["Date:", "11/5/2013"]
+	 },
+	 "stomp20": {
+		 "group":["Group:", "Arrival"],
+		 "sure":["Sure:", "3"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Talladaga"],
+		 "description":["Description:", "Bus"],
+		 "time":["Time:","2:15"],
+		 "date":["Date:","3/22/2013"]
+	 }	 
+ }
